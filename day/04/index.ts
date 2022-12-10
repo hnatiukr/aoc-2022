b0d10f3639e3681d228d43010c0b48d56be4e20a
Roman Hnatiuk <roman.hnatiuk@openvpn.net>
2022-12-10 22:40:16 +0200

    day(04): Camp Cleanup

--- day/04/index.ts
+++ day/04/index.ts
@@ -5,53 +5,52 @@ type AssignmentPairs = string[];
 type RangeTuple = [number, number];
 
 function makeRangeTuple(pair: string): RangeTuple {
-  return pair.split('-').map((x) => Number.parseInt(x, 10)) as RangeTuple;
+    return pair.split('-').map((x) => Number.parseInt(x, 10)) as RangeTuple;
 }
 
 function isPairWithin([a1, a2]: RangeTuple, [b1, b2]: RangeTuple): boolean {
-  return (a1 <= b1 && a2 >= b2) || (b1 <= a1 && b2 >= a2);
+    return (a1 <= b1 && a2 >= b2) || (b1 <= a1 && b2 >= a2);
 }
 
 function isPairOverlapped([a1, a2]: RangeTuple, [b1, b2]: RangeTuple): boolean {
-  return a2 < b1 || b2 < a1 ? false : true;
+    return a2 < b1 || b2 < a1 ? false : true;
 }
 
 //
 
 function part1(assignmentPairs: AssignmentPairs): void {
-  let count = 0;
+    let count = 0;
 
-  for (let index = 0; index < assignmentPairs.length; index += 1) {
-    const pairs = assignmentPairs[index];
-    const [tupleA, tupleB] = pairs.split(',').map(makeRangeTuple);
+    for (let index = 0; index < assignmentPairs.length; index += 1) {
+        const pairs = assignmentPairs[index];
+        const [tupleA, tupleB] = pairs.split(',').map(makeRangeTuple);
 
-    if (isPairWithin(tupleA, tupleB)) {
-      count += 1;
+        if (isPairWithin(tupleA, tupleB)) {
+            count += 1;
+        }
     }
-  }
 
-  console.log(count);
+    console.log(count);
 }
 
 function part2(assignmentPairs: AssignmentPairs): void {
-  let count = 0;
+    let count = 0;
 
-  for (let index = 0; index < assignmentPairs.length; index += 1) {
-    const pairs = assignmentPairs[index];
-    const [tupleA, tupleB] = pairs.split(',').map(makeRangeTuple);
+    for (let index = 0; index < assignmentPairs.length; index += 1) {
+        const pairs = assignmentPairs[index];
+        const [tupleA, tupleB] = pairs.split(',').map(makeRangeTuple);
 
-    if (isPairOverlapped(tupleA, tupleB)) {
-      count += 1;
+        if (isPairOverlapped(tupleA, tupleB)) {
+            count += 1;
+        }
     }
-  }
 
-  console.log(count);
+    console.log(count);
 }
 
 //
 
-const input = readInput('04');
-const assignmentPairs = input.split('\n');
+const assignmentPairs = readInput('04');
 
 part1(assignmentPairs);
 part2(assignmentPairs);
