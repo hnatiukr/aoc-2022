d1465d39fd667d56144ea2802600986ff607977a
Roman Hnatiuk <roman.hnatiuk@openvpn.net>
2022-12-10 22:31:43 +0200

    day(01): Calorie Counting

--- day/01/index.ts
+++ day/01/index.ts
@@ -3,49 +3,48 @@ import { readInput } from '../../utils.js';
 type Calories = string[];
 
 function part1(calories: Calories): void {
-  let count = 0;
-  let sum = 0;
+    let count = 0;
+    let sum = 0;
 
-  calories.forEach((cal) => {
-    if (cal) {
-      count += Number.parseInt(cal);
-    } else {
-      if (count > sum) {
-        sum = count;
-      }
+    calories.forEach((cal) => {
+        if (cal) {
+            count += Number.parseInt(cal);
+        } else {
+            if (count > sum) {
+                sum = count;
+            }
 
-      count = 0;
-    }
-  });
+            count = 0;
+        }
+    });
 
-  console.log(sum);
+    console.log(sum);
 }
 
 function part2(calories: Calories): void {
-  let count = 0;
+    let count = 0;
 
-  const top: Array<number> = [];
+    const top: Array<number> = [];
 
-  calories.forEach((cal) => {
-    if (cal) {
-      count += Number.parseInt(cal);
-    } else {
-      top.push(count);
+    calories.forEach((cal) => {
+        if (cal) {
+            count += Number.parseInt(cal);
+        } else {
+            top.push(count);
 
-      count = 0;
-    }
-  });
+            count = 0;
+        }
+    });
 
-  const mostCalories = top
-    .sort()
-    .slice(-1 * 3)
-    .reduce((sum, cal) => (sum += cal), 0);
+    const mostCalories = top
+        .sort()
+        .slice(-1 * 3)
+        .reduce((sum, cal) => (sum += cal), 0);
 
-  console.log(mostCalories);
+    console.log(mostCalories);
 }
 
-const input = readInput('01');
-const calories: Calories = input.split('\n');
+const calories = readInput('01');
 
 part1(calories);
 part2(calories);
